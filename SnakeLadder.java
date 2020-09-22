41ee401afbc6dda5232da39a206afedad592b255
Aditya Verma <aaadarths2357@gmail.com>
2020-09-22 21:37:02 +0530

    Use Case 2 : Dice Roll Operation

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,5 +4,7 @@ public class SnakeLadder{
 	public static void main(String[] args){
 
 		System.out.println("Player at Start Position "+startPosition);
+		int diceRoll=((int)Math.floor(Math.random()*10)%6)+1;
+		System.out.println("Dice Roll : "+diceRoll);
 	}
 }
