a9e2e5bae9dff7117a52554515970fdd8331d0dd
Aditya Verma <aaadarths2357@gmail.com>
2020-09-22 21:34:47 +0530

    Use Case 1 : Single Player at start position 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,7 +1,8 @@
 public class SnakeLadder{
-
+	
+	public static final int startPosition=0;
 	public static void main(String[] args){
 
-		System.out.println("Welcome to Sanke and Ladder game");
+		System.out.println("Player at Start Position "+startPosition);
 	}
 }
