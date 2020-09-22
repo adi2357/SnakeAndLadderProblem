ec3c3128dec35231b7c2ea86bd64dff2cfe5fd1a
adi2357 <54854466+adi2357@users.noreply.github.com>
2020-09-22 18:56:44 +0530

    Update SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,6 +2,6 @@ public class SnakeLadder{
 
 	public static void main(String[] args){
 
-		System.out.println("Welcome to Sanke and Ladder game");
+		System.out.println("Welcome to Snake and Ladder game");
 	}
 }
